45314a0734cec207329dd618c42a690ad5f6e144
Ivan Nikanau <somemore17@gmail.com>
2023-10-11 15:18:29 +0300

    Finish section 7 - Model-View-Controller

---
+++ models/product.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
+
+module.exports = class Product {
+  constructor(title, price, thumbnail) {
+    this.title = title;
+    this.price = price;
+    this.thumbnail = thumbnail;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
