e97212af67731d803f25777db5b964e541c691c6
Ivan Nikanau <somemore17@gmail.com>
2023-10-16 13:30:16 +0300

    Start section 12 - Using MongoDB

--- util/database.js
+++ util/database.js
@@ -1,9 +1,20 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
 
-const sequelize = new Sequelize("course-nodejs", "root", "admin", {
-  dialect: "mysql",
-  host: "localhost",
-  logging: false,
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+// const uri = "mongodb+srv://admin:pgZzV5DW0KevCQ9o@cluster0.ovhxueq.mongodb.net/test?retryWrites=true&w=majority"
+const uri =
+  "mongodb+srv://admin:2Nn55knHaNhWN0F0@cluster0.zxtdm2i.mongodb.net/?retryWrites=true&w=majority";
+
+const mongoConnect = (cb) => {
+  MongoClient.connect(uri)
+    .then((client) => {
+      console.log("Connected");
+      cb(client);
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
+};
+
+module.exports = mongoConnect;
