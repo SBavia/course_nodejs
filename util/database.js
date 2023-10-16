562081df41062d6257b0f034f3c7795f0618b4cd
Ivan Nikanau <somemore17@gmail.com>
2023-10-16 13:42:32 +0300

    fix

--- util/database.js
+++ util/database.js
@@ -2,9 +2,7 @@ const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
-// const uri = "mongodb+srv://admin:pgZzV5DW0KevCQ9o@cluster0.ovhxueq.mongodb.net/test?retryWrites=true&w=majority"
-const uri =
-  "mongodb+srv://admin:2Nn55knHaNhWN0F0@cluster0.zxtdm2i.mongodb.net/?retryWrites=true&w=majority";
+const uri = "";
 
 const mongoConnect = (cb) => {
   MongoClient.connect(uri)
