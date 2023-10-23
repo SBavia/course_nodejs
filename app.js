953ea85d37d019f56be8b08b34648cd09aa23cf6
Ivan Nikanau <ivan.nikanau@pm.me>
2023-10-23 20:24:27 +0300

    Working on section 12 - Using MongoDB

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+require("dotenv").config();
 const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database");
+const mongoConnect = require("./util/database").mongoConnect;
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// const adminRoutes = require("./routes/admin");
-// const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -24,15 +25,14 @@ app.use((req, res, next) => {
   //     next();
   //   })
   //   .catch((err) => console.log(err));
-  // // next();
+  next();
 });
 
-// app.use("/admin", adminRoutes);
-// app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client) => {
-  console.log(client);
+mongoConnect(() => {
   app.listen(3000);
 });
--- util/database.js
+++ util/database.js
@@ -2,17 +2,29 @@ const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
-const uri = "";
+const uri = process.env.MONGODB_URI;
+
+let _db;
 
 const mongoConnect = (cb) => {
   MongoClient.connect(uri)
     .then((client) => {
       console.log("Connected");
-      cb(client);
+      _db = client.db();
+      cb();
     })
     .catch((err) => {
       console.log("err", err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDB = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
