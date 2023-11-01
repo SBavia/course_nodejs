fd2f1002a6e149efb270049a84f3e09091f82827
Ivan Nikanau <ivan.nikanau@pm.me>
2023-11-01 20:16:55 +0200

    Finish section 22 - Adding Payments

--- src/controllers/shop.js
+++ src/controllers/shop.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 const PDFDocument = require("pdfkit");
 
 const Product = require("../models/product");
@@ -141,7 +142,52 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
-exports.postOrder = (req, res, next) => {
+exports.getCheckout = (req, res, next) => {
+  let products,
+    totalSum = 0;
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      products = user.cart.items;
+      products.forEach((p) => {
+        totalSum += p.quantity * p.productId.price;
+      });
+
+      return stripe.checkout.sessions.create({
+        line_items: products.map((p) => ({
+          price_data: {
+            currency: "usd",
+            unit_amount: p.productId.price * 100,
+            product_data: {
+              name: p.productId.title,
+              description: p.productId.description,
+            },
+          },
+          quantity: p.quantity,
+        })),
+        mode: "payment",
+        success_url:
+          req.protocol + "://" + req.get("host") + "/checkout/success",
+        cancel_url: req.protocol + "://" + req.get("host") + "/checkout/cancel",
+      });
+    })
+    .then((session) => {
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        sessionId: session.id,
+        products,
+        totalSum,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
   req.user
     .populate("cart.items.productId")
     .then((user) => {
