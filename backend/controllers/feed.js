bc69b7a3384f24ce50e18c86ad6ac312d3d9dfc6
Ivan Nikanau <ivan.nikanau@pm.me>
2023-11-02 20:50:56 +0200

    Working on section 25 - Advanced REST APIs

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,19 +1,44 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the first post" }],
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post",
+        imageUrl: "images/image.jpg",
+        creator: {
+          name: "John Doe",
+        },
+        createdAt: new Date().toISOString(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect.",
+      errors: errors.array(),
+    });
+  }
+
   const title = req.body.title;
   const content = req.body.content;
 
   res.status(201).json({
     message: "Post created successfully!",
     post: {
+      _id: new Date(),
       title,
       content,
-      id: new Date().toISOString(),
+      creator: {
+        name: "John Doe",
+      },
+      createdAt: new Date().toISOString(),
     },
   });
 };
